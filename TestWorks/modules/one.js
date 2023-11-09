fbca4d19925f23926ea37c72b1ce325d6456929e
RareMashiro <symon.belmont2015@yandex.ru>
2023-11-09 20:59:23 +0300

    Modules files eddited!

--- TestWorks/modules/one.js
+++ TestWorks/modules/one.js
@@ -1,9 +1,10 @@
 import {multiply} from './two.js';
-import { createObject } from './two.js';
-import { getUrl } from './two.js';
+import {createObject} from './two.js';
+export {getUrl} from './two.js';
+export {default as Help} from './two.js';
+
+// ! реэкспортируемые файлы не инициализируются здесь
 
 let obj = new createObject(11, 14);
 
 multiply(obj.first, obj.second);
-
-getUrl();
--- TestWorks/modules/two.js
+++ TestWorks/modules/two.js
@@ -6,9 +6,30 @@ export class createObject {
     constructor(first, second) {
         this.first = first;
         this.second = second;
+        alert('Success!');
     }
 }
 
 export function getUrl() {
-    alert(import.meta.url);
+    return import.meta.url;
+}
+
+export default async function() {
+    let response = await fetch('https://api.github.com/users/RareMashiro');
+    let user = await response.json();
+    let avatar = user.avatar_url;
+
+    let img = document.createElement('img');
+    img.src = avatar;
+    document.body.append(img);
+
+    let p = document.createElement('p');
+    p.textContent = user.login;
+    document.body.append(p);
+    
+    setTimeout(() => {
+        img.remove();
+        p.remove();
+    }, 4000);
+
 }
