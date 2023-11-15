97aaf5dffa69eb2a91b26bf00ce6bc3794fb0e0a
RareMashiro <symon.belmont2015@yandex.ru>
2023-11-15 20:06:01 +0300

    React files added!

---
+++ React/my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { FilmDetails } from "./components/FilmDetails/FilmDetails"
+
+export const AppCreate = () => {
+    return (
+        <div>
+            <header />
+            <FilmDetails 
+                title={FilmDetails.title} 
+                seasonsCount={FilmDetails.seasonsCount} 
+                genre={FilmDetails.genre} 
+            />
+
+            <footer />
+        </div>
+    )
+}
